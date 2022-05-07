4535e8093d6cd5bc764a33febd60cb3154d6e0ff
Davi Souza Sande <davisande@gmail.com>
2022-05-07 17:10:09 -0300

    Divisible sum pairs chalenge

---
+++ src/hackerrankchalenge/DivisibleSumPairs.java
@@ -0,0 +1,29 @@
+package hackerrankchalenge;
+
+import java.util.List;
+import java.util.stream.IntStream;
+
+public class DivisibleSumPairs {
+
+    public static void main(String[] args) {
+        List<Integer> ar = List.of(1, 3, 2, 6, 1, 2);
+        System.out.println(divisibleSumPairs(ar.size(), 3, ar));
+    }
+
+    public static int divisibleSumPairs(int n, int k, List<Integer> ar) {
+        return IntStream.range(0, n)
+                .map(i -> checkDivisiblePairs(i, n, k, ar))
+                .sum();
+    }
+
+    public static int checkDivisiblePairs(int i, int n, int k, List<Integer> ar) {
+        var num1 = ar.get(i);
+
+        long result = IntStream.range((i + 1), n)
+                .map(j -> num1 + ar.get(j))
+                .filter(r -> r % k == 0)
+                .count();
+
+        return Math.toIntExact(result);
+    }
+}
